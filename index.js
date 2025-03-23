d559cd0902f4658f09b04864b12bbdffee430fb9
irfan420x <marufmdirfanahmed@gmail.com>
2025-03-23 22:17:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const express = require('express');
 const Constants = require('./src/util/Constants');
 
+const app = express();
+const port = 3000; // আপনি চাইলে এটি পরিবর্তন করতে পারেন
+
+// API বা সার্ভারের জন্য রাউট যুক্ত করুন
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// মূল মডিউল রপ্তানি
 module.exports = {
     Client: require('./src/Client'),
     
@@ -31,3 +41,8 @@ module.exports = {
     
     ...Constants
 };
+
+// সার্ভার চালানো
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
